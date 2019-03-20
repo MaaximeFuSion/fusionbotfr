69e3a5ad75ca9dd079f4ae381c314b9891f239a3
YannFusion <46075526+YannFusion@users.noreply.github.com>
2019-03-20 20:16:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,20 +2,27 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+const client = new Discord.Client();
+
+var PREFIX = ".";
+
 bot.on("ready", function() {
     bot.user.setGame("V1.1 | .help");
-    client.user.setStatus("dnd");
+    bot.user.setStatus("dnd");
 });
 
-bot.login(process.env.TOKEN);
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
 
-client.on('message', message =>
-{
+    if(!message.content.startsWith(PREFIX)) return;
 
-//serverlist
-if(message.content === '.test')
-{
-    message.channel.sendMessage('Allons au bout des choses... :D');
-}
+    var args = message.content.substring(PREFIX.length).split(" ");
 
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("Test");
+        break;
+    }
 });
+
+bot.login("NTU3OTcyODEzMjk4Nzk0NDk3.D3QEcg.A16Qk71OEz-cYUQf-2Y7gR4LbC8");
