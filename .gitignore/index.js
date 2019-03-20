e78b3fa65fea3b9207c0b3ba8c726182b10986cb
YannFusion <46075526+YannFusion@users.noreply.github.com>
2019-03-20 18:46:22 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,17 +3,17 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("FS Team | .help");
+    client.user.setActivity("V1.1 Bêta | .help", { type: "STREAMING", url: "https://www.twitch.tv/yannfusionfr" })
     console.log("Bot > ON");
 });
 
-bot.login(process.env.TOKEN)
+bot.login(process.env.TOKEN);
 
 client.on('message', message =>
 {
 
 //serverlist
-if(message.content === '/serverlist')
+if(message.content === '.serverlist')
 {
     message.channel.send(client.guilds.map(r => "**" + r.name + `** | **${r.memberCount}** member | **` + r.owner + '**'))
 }
