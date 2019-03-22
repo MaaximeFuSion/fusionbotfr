0ffb45e3f79ead8b18f220692609b0371e7b7ebe
MaaximeFuSion <48837661+MaaximeFuSion@users.noreply.github.com>
2019-03-22 18:19:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -62,5 +62,56 @@ bot.on("message", async function(message) {
     }
 });
 
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "mod":
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.username,
+                title: 'mod',
+                description: 'Commande non disponible',
+                fields: [{
+                    name: "Accès refusé",
+                    value: "Accès requis : **MOD**",
+                    inline: false
+                }]
+        }
+        });
+        break;
+    }
+});
+
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "modhelp":
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.username,
+                title: 'modhelp',
+                description: 'Commande non disponible',
+                fields: [{
+                    name: "Accès refusé",
+                    value: "Accès requis : **MOD**",
+                    inline: false
+                }]
+        }
+        });
+        break;
+    }
+});
 
 bot.login(process.env.TOKEN);
